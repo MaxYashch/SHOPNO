d4f83f8e1db32824aa3f0afff339d994220279f6
MaxYashch <maxyashch@gmail.com>
2020-10-02 21:18:05 +0300

    feated messages and work areas

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,11 +1,3 @@
-// function myMap() {
-// var mapProp= {
-//   center:new google.maps.LatLng(51.508742,-0.120850),
-//   zoom:5,
-// };
-// var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-// }
-
 ymaps.ready(init);
 function init() {
     // Создание карты.
